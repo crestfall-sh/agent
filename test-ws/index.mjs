4b8b70800055bcedff5fff9b41989e46d831973f
joshxyzhimself <joshxyzhimself@gmail.com>
2023-03-22 17:12:03 +0800

    add test websocket server

---
+++ test-ws/index.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import assert from 'assert';
+import uws from 'uws';
+
+const app = uws.App({});
+
+app.ws('/*', {
+  open: (ws) => {
+    console.log('uws: open');
+  },
+  message: (ws, message, is_binary) => {
+    console.log('uws: message');
+    if (is_binary === false) {
+      console.log(Buffer.from(message).toString(), is_binary);
+    }
+  },
+  close: (ws) => {
+    console.log('uws: close');
+  },
+});
+
+app.get('/*', (res) => {
+  res.writeStatus('200');
+  res.writeHeader('Content-Type', 'text/plain');
+  res.write('Hello world!');
+  res.end();
+});
+
+app.listen(8080, 1, (token) => {
+  assert(token !== false, 'uws: app.listen failed.');
+  console.log('uws: listening at port 8080');
+});
